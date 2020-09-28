68d499f9bec597fcfdcb99580254a1f3d4127bda
腾哥 <DELL@192.168.134.1>
2020-09-28 17:08:30 +0800

    wugong

---
+++ wuxia/src/com/rc/Wugong.java
@@ -0,0 +1,11 @@
+package com.rc;
+
+public class Wugong {
+	public static void main(String[] args) {
+		System.out.println("11111");
+		System.out.println("22222");
+		System.out.println("33333");
+		System.out.println("AAAAA");
+	}
+
+}
